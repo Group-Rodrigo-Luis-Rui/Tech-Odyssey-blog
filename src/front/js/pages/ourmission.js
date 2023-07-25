6813712e36fa2b02ac65b224c4ea625d6175c047
Rui Silva <rsilva6420@gmail.com>
2023-07-25 16:04:37 +0000

    font family changed

--- src/front/js/pages/ourmission.js
+++ src/front/js/pages/ourmission.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ourmission_image from "../../img/our_mission_image.jpg";
+import ourmission_image from "/workspaces/4Geeks-Final-Project-Team-1/src/front/img/our_mission_image.jpg";
 import "../../styles/ourmission.css";
 
 export const OurMission = () => {
 
 	return (
 		<div className="container">
-			
 			<h2><strong>Tech Odyssey</strong>: Exploring the Cutting-Edge Convergence of Computers, Movie Tech, AI, EV Mobility, and More!</h2><br/>
 			<p><em>Welcome to our Tech Odyssey - Where Boundaries Blur and Innovations Unite!</em></p>
 			<p>In the ever-evolving landscape of technology, groundbreaking advancements are revolutionizing every aspect of our lives. From the fascinating world of computers to the captivating magic of movie tech, the limitless potential of artificial intelligence, and the eco-conscious realm of electric vehicle (EV) mobility, there's an array of exciting topics to explore. Join us on a journey through the realm of technology, where barriers between different domains dissolve, and extraordinary synergies are discovered.</p>
 			<h3>Beyond Binary: Unraveling the Future of Computers</h3>
-			<h4>Tech Odyssey Team</h4>
 			<p>Computing technology continues to expand at an exponential pace. Quantum computing, once a dream, is becoming a reality, promising unprecedented computational power that will transform industries, from drug discovery to cryptography. Dive into the quantum realm and learn how it's changing the landscape of computing.</p>
 			<p>Meanwhile, cloud computing continues its ascendancy, offering businesses and consumers unparalleled flexibility and scalability. We'll explore how cloud-based services are transforming the way we work, communicate, and store data securely.</p>
 			<h3>Movie Tech: Unleashing the Art of Immersive Storytelling</h3>
@@ -27,16 +25,16 @@ export const OurMission = () => {
 			<h3>Uncharted Frontiers: Exploring the Coalescence of Tech and Beyond</h3>
 			<p>In this section, we'll venture into the uncharted territories where technology intertwines with unexpected disciplines. Discover the intersection of tech and agriculture, exploring how AI and data analytics are transforming farming practices to ensure food security for a growing global population.</p>
 			<p>We'll also dive into the world of augmented reality (AR) and virtual reality (VR), where creativity knows no bounds, and boundaries between real and digital experiences blur.</p>
-			<br/>
+
 			<hr/>
 			<hr/>
-			<br/>
+
 			<p>Join us on this thrilling tech adventure, where we'll unravel the mysteries of cutting-edge innovations, examine how technology is reshaping various industries, and envision a future where the boundaries between different tech categories dissolve, giving rise to a world of boundless possibilities. Stay tuned for mind-boggling insights, expert opinions, and thought-provoking discussions, right here on our Tech Odyssey blog!</p>
-			<h4>Tech Odyssey Team</h4>
+			<h3>Tech Odyssey Team</h3>
 			<ul>
-				<li>Luís Santos</li>
-				<li>Rui Silva</li>
-				<li>Rodrigo Almeida</li>
+				<li>Luís Santos <i class="fab fa-facebook"></i>  <i class="fab fa-linkedin"></i>  <i class="fab fa-twitter"></i>  <i class="fab fa-instagram"></i></li>
+				<li>Rui Silva <i class="fab fa-facebook"></i>  <i class="fab fa-linkedin"></i>  <i class="fab fa-twitter"></i>  <i class="fab fa-instagram"></i></li>
+				<li>Rodrigo Almeida <i class="fab fa-facebook"></i>  <i class="fab fa-linkedin"></i>  <i class="fab fa-twitter"></i>  <i class="fab fa-instagram"></i></li>
 			</ul>
 			<Link to = {"/"}><span>Home</span></Link>
 		</div>
