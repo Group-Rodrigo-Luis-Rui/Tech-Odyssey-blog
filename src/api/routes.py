ebb124a5ff464315460f51792e480c04c66f1242
Rui Silva <rsilva6420@gmail.com>
2023-08-02 08:24:51 +0000

    initial commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,6 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
@@ -16,3 +17,44 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+
+@api.route('/login', methods=['POST'])
+def login_user():
+
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    if email is None:
+        response_body = {
+            "message": "Email does not exist"
+        }
+        return jsonify(response_body), 400
+    
+    if password is None:
+        response_body = {
+            "message": "Password does not exist"
+        }
+        return jsonify(response_body), 400
+    
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        # the user was not found on the database
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    # create a new token with the user id inside
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
+
+
+@api.route('/tasks', methods=['GET'])
+@jwt_required()
+def get_tasks():
+
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    res_user = user.serialize()
+
+    print(' Authenticated Users Data', user)
+    return jsonify(res_user), 200
