070a026aa91597ee73055e5bcae29dff58ad99e1
Rui Silva <rsilva6420@gmail.com>
2023-08-04 11:04:11 +0000

    change some text and button color to match navbar, input cursor pointer fixed

--- src/front/js/pages/createpost.js
+++ src/front/js/pages/createpost.js
@@ -13,12 +13,12 @@ export const Createpost = () => {
 						<h1>Create post</h1>
 						<form action="" method="POST">
 							<div className="form-group mb-4">
-								<label for="title" className="mb-1 fs-5 text-info">Title</label>
+								<label for="title" className="mb-1 titlesText">Title</label>
 								<input type="text" className="form-control textAreaCreate" name="title" placeholder="Max 100 characters" />
 							</div>
 							<div className="form-group mb-4">
-								<label for="exampleFormControlSelect1" className="mb-1 fs-5 text-info">Select category</label>
-								<select className="form-control textAreaCreate" id="exampleFormControlSelect1">
+								<label for="exampleFormControlSelect1" className="mb-1 titlesText">Select category</label>
+								<select className="form-control textAreaCreate optionText" id="exampleFormControlSelect1">
 									<option>Choose one</option>
 									<option>Computers</option>
 									<option>Movie Tech</option>
@@ -28,26 +28,26 @@ export const Createpost = () => {
 								</select>
 							</div>
 							<div className="form-group mb-4">
-								<label for="description" className="mb-1 fs-5 text-info">Abstract</label>
+								<label for="description" className="mb-1 titlesText">Abstract</label>
 								<textarea rows="4" className="form-control textAreaCreate" name="description" placeholder="Max 300 characters"></textarea>
 							</div>
 							<div>
 								<div class="d-flex justify-content-left mb-4">
 									<div class="btn btn-light btn-rounded buttonCreate">
-										<label class="form-label text-secondary m-1" for="customFile1">Choose file</label>
+										<label class="form-label m-1" for="customFile1">Choose file</label>
 										<input type="file" className="form-control d-none" id="customFile1" />
 									</div>
 									<div>
-										<p class="fs-5 text-info pt-2 ms-3">Insert image</p>
+										<p class="pt-2 ms-3 titlesText">Insert image</p>
 									</div>
 								</div>
 							</div>
 							<div className="form-group mb-4">
-								<label for="description" className="mb-1 fs-5 text-info">My text</label>
+								<label for="description" className="mb-1 titlesText">My text</label>
 								<textarea rows="50" className="form-control textAreaCreate" name="description" placeholder="Max 5000 characters"></textarea>
 							</div>
 							<div className="form-group d-flex justify-content-center">
-								<button type="submit" className="btn btn-info me-5">
+								<button type="submit" className="btn me-5 buttonCreate">
 									Create
 								</button>
 								<button className="btn btn-default btn-danger">
