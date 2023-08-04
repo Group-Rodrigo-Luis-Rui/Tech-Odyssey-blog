4368be0fdaa328c719af05ac78c4e92daf7391e3
Rodrigou0 <127239242+Rodrigou0@users.noreply.github.com>
2023-08-04 17:33:58 +0000

    single

--- src/front/js/pages/single.js
+++ src/front/js/pages/single.js
@@ -9,16 +9,17 @@ export const Single = props => {
 	const params = useParams();
 
 	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
-			<img src={rigoImageUrl} />
-			<hr className="my-4" />
+		<div className="container-fluid">
+			<div class="card" style="width: 18rem;">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					<a href="#" class="card-link">Card link</a>
+					<a href="#" class="card-link">Another link</a>
+				</div>
+			</div>
 
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
 		</div>
 	);
 };
