f1e5f8d6fb2cc1e7986833d70ea8e81c11e386ae
Rui Silva <rsilva6420@gmail.com>
2023-08-11 19:17:47 +0000

    just scratch

--- src/front/js/pages/myprofile.js
+++ src/front/js/pages/myprofile.js
@@ -2,8 +2,34 @@ import React from "react";
 import backgroundimage from "../../img/backgroundimage.jpg";
 import avatarImage from "../../img/rigo-baby.jpg";
 import "../../styles/myprofile.css";
+import { token } from "stylis";
 
 export const MyProfile = () => {
+
+	const getOneUser = () => {
+        fetch(process.env.BACKEND_URL + "/api/user/", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+				// "Authorization": "Bearer " + token;
+            }
+        })
+        .then(resp => {
+            console.log(resp.ok); // will be true if the response is successfull
+            console.log(resp.status); // the status code = 200 or code = 400 etc.
+            console.log(resp); // will try return the exact result as string
+            return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+        })
+        .then(data => {
+            //here is where your code should start after the fetch finishes
+            console.log(data); //this will print on the console the exact object received from the server
+        })
+        .catch(error => {
+            //error handling
+            console.log(error);
+        });
+    }
+
 	const user = {
 		name: "John Doe",
 		email: "john@example.com",
