5e8020e49c655eda62efdd61ec117df63937f47e
Rodrigou0 <127239242+Rodrigou0@users.noreply.github.com>
2023-09-14 20:42:00 +0000

    single

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -397,7 +397,8 @@ export const Home = () => {
 									<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 									<a href="#" className="btn btn-primary">Go somewhere</a>
 								</div>
-							</div>
+								{Title3}
+						</MovingComponent>
 					))}
 				</div>
 				{/* scrolling cards*/}
